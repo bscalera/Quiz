7ef9f1c910bcdc5619f26feb6c3bde75f8fc15f8
bscalera <Scalerab1@mail.montclair.edu>
2017-10-17 15:22:41 -0400

    Final version of quiz.py
    This was the final version of the homework which was saved into the wrong place.

--- quiz.py
+++ quiz.py
@@ -1,3 +1,6 @@
+questionNumber=10
+question=[None for x in range(questionNumber)]
+number=0
 print("Here is a quiz to see which power ranger you are")
 
 print("Question 1")
@@ -6,8 +9,9 @@ print("a.Tyrannosaurus")
 print("b.Triceratops")
 print("c.Saber-Toothed Tiger")
 print("d.Pterodactyl")
-if answer1 =="A,B,C,D":
-    score = score+1
+question[number]= input("Answer: ")
+number+=1 #n= n+1   
+
 # Ask the question and get an answer  
 print("Question 2")
 print("Which color do you like best out of this list?")
@@ -15,6 +19,8 @@ print("a.Blue")
 print("b.Red")
 print("c.Yellow")
 print("d.Pink")
+question[number]= input("Answer: ")
+number+=1
 
 print("Question 3")
 print("Which childhood show do you like from the list?")
@@ -22,6 +28,8 @@ print("a.Rocket Power")
 print("b.Power Rangers")
 print("c.Spongebob Squarepants")
 print("d.Barney")
+question[number]= input("Answer: ")
+number+=1
 
 print("Question 4")
 print("Which place would you go to on vacation?")
@@ -29,47 +37,64 @@ print("a.Hawaii")
 print("b.Bahamas")
 print("c.Florida")
 print("d.California")
-print()
+question[number]= input("Answer: ")
+number+=1
+
 print("Question 5")
 print("Favorite car brand?")
 print("a.Ford")
 print("b.Nissan")
 print("c.BMW")
 print("d.Honda")
-print()
+question[number]= input("Answer: ")
+number+=1
+
 print("Question 6")
 print("Favorite genre of music")
 print("a.Rap")
 print("b.Rock")
 print("c.Country")
 print("d.EDM")
-print()
+question[number]= input("Answer: ")
+number+=1
+
 print("Question 7")
 print("Favorite subject in school")
 print("a.Math")
 print("b.Gym")
 print("c.History")
 print("d.Art")
-print()
+question[number]= input("Answer: ")
+number+=1
+
 print("Question 8")
 print("Favorite movie series")
 print("a.Transformers")
 print("b.Twilight")
 print("c.Fast and the Furious")
 print("d.Spider Man")
-print()
+question[number]= input("Answer: ")
+number+=1
+
 print("Question 9")
 print("Favorite super hero")
 print("a.Thor")
 print("b.Ant man")
 print("c.The Flash")
 print("d.Iron Man")
-print()
+question[number]= input("Answer: ")
+number+=1
+
 print("Question 10")
 print("Favorite sport")
 print("a.Soccer")
 print("b.Basketball")
 print("c.Hockey")
 print("d.Baseball")
+question[number]= input("Answer: ")
+number+=1
+
+#for x in question:
+    #print question[x]
 
 input("\nPress the enter key to exit.")
